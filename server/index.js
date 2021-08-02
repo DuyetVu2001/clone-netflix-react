0018695282a8fe03cb73cd177dd6d91c6a70e716
DuyetVu2001 <vungocduyet24@gmail.com>
2021-08-02 12:50:19 +0700

    add cors package

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const route = require('./routes');
 
@@ -11,6 +12,7 @@ const port = 8800;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 route(app);
 
 mongoose
